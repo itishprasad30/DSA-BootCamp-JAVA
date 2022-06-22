10ce0d4ccf594d5d858e53fdad640fc2434f88a1
itishprasad30 <itishprasad20@gmail.com>
2022-06-22 22:25:28 +0530

    input file added

---
+++ lectures/05-first-java-program/first-intelij-program/src/com/itish/primitive.java
@@ -0,0 +1,15 @@
+package com.itish;
+
+import java.util.Scanner;
+
+public class primitive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please input some value Here : ");
+        String name = input.nextLine();
+        System.out.println(name);
+
+
+    }
+}
