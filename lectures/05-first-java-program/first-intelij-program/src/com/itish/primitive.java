3ce413a69acbd7f51414e46380580a9aeed7f0aa
itishprasad30 <itishprasad20@gmail.com>
2022-06-23 09:12:02 +0530

    all primitive files are added

--- lectures/05-first-java-program/first-intelij-program/src/com/itish/primitive.java
+++ lectures/05-first-java-program/first-intelij-program/src/com/itish/primitive.java
@@ -1,14 +1,32 @@
 package com.itish;
 
-import java.util.Scanner;
 
 public class primitive {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // Numbers
+        byte bb = 12;
+        short ss = 32;
+        int ii = 4321;
+        long ll = 1234998532224442344L;
 
-        System.out.print("Please input some value Here : ");
-        String name = input.nextLine();
-        System.out.println(name);
+        // Floating type data
+        float ff = 432.93f;
+        double dd = 3288492.1294820;
+
+        // charcter
+        char ch = 'y';
+
+        // boolean
+        boolean bol = true;
+        // String
+
+        String name = "Pintu sahoo";
+
+        System.out.println(
+                bb+ "\n"+ ss + "\n" + ii +"\n" + ll +"\n" + ff +"\n" + dd + "\n" + ch + "\n" + bol + "\n" + name
+        );
+
+
 
 
     }
