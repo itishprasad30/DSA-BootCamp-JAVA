b93576f55af23a44bcc4e034b785fe0a48565719
itishprasad30 <itishprasad20@gmail.com>
2022-07-10 15:38:06 +0530

    BinarySearch foumula added

--- lectures/10-binary-search/code/src/com/itish/BinarySearch.java
+++ lectures/10-binary-search/code/src/com/itish/BinarySearch.java
@@ -2,9 +2,32 @@ package com.itish;
 
 public class BinarySearch {
     public static void main(String[] args) {
+        // asumming sorted array
+        int [] arr ={2,4,6,8,10,45,78,98};
+        int target = 98;
+        int result = binarySearch(arr,target);
+        System.out.println("The Element found at index no :" + result);
 
     }
     static int binarySearch(int[] arr,int target){
+        // 1. find the mid element
+        int start =0;
+        int end = arr.length -1;
+        while (start<=end){
+            // find mid element
+            // this might cause int exceed issue;
+            //  int mid =( start - end )/ 2;
+            int mid = start + (end -start) /2;
 
+            if(target < arr[mid]){
+                end = mid -1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            }else {
+                return mid;// ans found
+            }
+        }
+        // if the element not found
+        return -1;
     }
 }
