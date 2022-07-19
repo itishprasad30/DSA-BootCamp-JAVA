c879d39aa13574d56f2eeb48def740c51d09f6ed
itishprasad30 <itishprasad20@gmail.com>
2022-07-19 21:24:52 +0530

    RotetedBS for duplicate values added the changes

--- lectures/10-binary-search/code/src/com/itish/RotetedSortedBS.java
+++ lectures/10-binary-search/code/src/com/itish/RotetedSortedBS.java
@@ -1,10 +1,14 @@
 package com.itish;
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
 
 public class RotetedSortedBS {
     public static void main(String[] args) {
         int [] arr = {4,5,6,7,0,1,2,3};
         int target = 3;
         System.out.println(search(arr,target));
+
+        int [] nums = {22,22,22,54,32,22,76,22};
+        System.out.println(findPivotDuplicateArr(nums));
     }
 
 
@@ -45,6 +49,9 @@ public class RotetedSortedBS {
         // if the element not found
         return -1;
     }
+
+    //this will not work in duplicate array
+
     // find the pivot- largest element in the array
     static int findPivot(int[] arr){
         int start=0;
@@ -57,7 +64,7 @@ public class RotetedSortedBS {
             if(mid< end && arr[mid] > arr[mid+1]){
                 return mid;
             }
-            if(mid > start && arr[mid] < arr[mid+1]){
+            if(mid > start && arr[mid] < arr[mid-1]){
                 return mid -1;
             }
             if(arr[mid] <= arr[start]){
@@ -68,6 +75,48 @@ public class RotetedSortedBS {
 
         }
         return -1;
-
     }
+
+    // for dublicate array Rotated Binary Search is
+    public  static  int findPivotDuplicateArr(int[] arr){
+        int start=0;
+        int end = arr.length -1;
+
+        while (start<= end){
+            int mid = start + (end-start )/2;
+
+            //cases for pivot
+
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(mid > start && arr[mid] < arr[mid-1]) {
+                return mid -1;
+            }
+
+            // if the element at start,middle,end are quals skip the duplicate
+
+            if(arr[mid] == arr[start] && arr[mid] == arr[end]){
+                // check start is pivot or not
+                // skip the duplicate
+                if(arr[start] > arr[start+1]) {
+                    return start;
+                }
+
+                start++;
+                if(arr[end] > arr[end-1]){
+                    return end-1;
+                }
+                end--;
+            }
+            // left side is sorted ,pivot should be in right side
+            else if (arr[start]<arr[mid] || (arr[start]==arr[mid] && arr[mid]>arr[end])) {
+                start = mid+1;
+            }else {
+                end = mid -1;
+            }
+        }
+        return -1;
+    }
+
 }
