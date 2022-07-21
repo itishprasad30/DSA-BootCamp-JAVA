5ca1aff8d4f11ac5a484fd6d6c53d030d94ad758
itishprasad30 <itishprasad20@gmail.com>
2022-07-21 21:09:30 +0530

    RotetedBS  count is added

--- lectures/10-binary-search/code/src/com/itish/RotationCountOnBS.java
+++ lectures/10-binary-search/code/src/com/itish/RotationCountOnBS.java
@@ -2,40 +2,38 @@ package com.itish;
 
 public class RotationCountOnBS {
     public static void main(String[] args) {
-        int [] nums = {5,6,1,2,3};
-        int ans = rotationCount(nums);
-        System.out.println(ans);
+        int [] arr = {1,2,4,5,6,7,0};
+        System.out.println(countRotation(arr));
 
     }
 
-   public static int rotationCount(int[] arr){
+    public static int countRotation(int[] arr) {
         int pivot = findPivot(arr);
         return pivot+1;
     }
-    static int findPivot(int[] arr){
-        int start = 0;
-        int end= arr.length -1;
+
+    public static int findPivot(int[] arr){
+        int start=0;
+        int end=arr.length -1;
 
         while (start<=end){
-            int mid = start + (end -start)/2;
-
-            if(mid < end && arr[mid] > arr[mid+1]){
+            int mid = start + (end-start)/2;
+            // if mid > mid+1 => pivot
+            if(mid< end && arr[mid] > arr[mid+1]){
                 return mid;
             }
-            if (mid<start && arr[mid] < arr[mid-1] ){
-                return mid-1;
+            if(mid > start && arr[mid] < arr[mid-1]){
+                return mid -1;
             }
             if(arr[mid] <= arr[start]){
-                end = mid -1;
+                end= mid-1;
             }else {
-                start = mid +1;
+                start = mid+1;
             }
+
         }
-
-
         return -1;
     }
-
 
 
 }
