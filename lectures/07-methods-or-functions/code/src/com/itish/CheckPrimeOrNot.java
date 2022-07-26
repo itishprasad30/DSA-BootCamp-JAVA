198943cab23117a18fb0ceeadf280f18f52174d1
itishprasad30 <itishprasad20@gmail.com>
2022-07-26 07:40:57 +0530

    palindrom check added

--- lectures/07-methods-or-functions/code/src/com/itish/CheckPrimeOrNot.java
+++ lectures/07-methods-or-functions/code/src/com/itish/CheckPrimeOrNot.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class CheckPrimeOrNot {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-//        int n = in.nextInt();
+        int n = in.nextInt();
+        System.out.println(isPlaindrom(n));
 
-//        boolean result = isPrime(n);
-//        System.out.println(result);
+        boolean result = isPrime(n);
+        System.out.println(result);
 
         for (int i = 0; i < 100; i++) {
             if(isPrime(i)){
@@ -22,6 +23,23 @@ public class CheckPrimeOrNot {
                 System.out.print(i + "\t");
             }
         }
+
+    }
+
+    static boolean isPlaindrom (int n){
+        if(n<0 || n % 10 == 0){
+            return false;
+        }
+        int orginal = n;
+        int rev = 0;
+        while (n> 0){
+            int rem = n %10;
+            n = n/10;
+            rev = rev *10 + rem;
+        }
+
+        return rev == orginal;
+
 
     }
     static boolean isAmstorng (int n) {
