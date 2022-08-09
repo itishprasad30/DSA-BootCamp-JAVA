59c34955706311f493adc99cf45868c517686092
itishprasad30 <itishprasad20@gmail.com>
2022-08-09 21:07:21 +0530

    basic program

--- lectures/155-Programming-different/code.itish/src/Conversion/Conversion.java
+++ lectures/155-Programming-different/code.itish/src/Conversion/Conversion.java
@@ -39,4 +39,28 @@ public class Conversion {
         return hx;
     }
 
+    public int binToDec(int bin) // 1101
+    {
+        int pw =1,dec =0;
+        do {
+            int r = bin %10;
+            dec = dec + r * pw;
+            pw = pw*2;
+            bin = bin/10;
+
+        }while (bin!=0);
+        return dec;
+    }
+
+    public int octToDec(int oct){
+        int pw=1,dec =0;
+        do {
+            int r = oct%10;
+            dec = dec + r *pw;
+            pw = pw*8;
+            oct = oct /10;
+        }while (oct!=0);
+        return dec;
+    }
+
 }
