e9f2db13d24bb6430b0b6fd0c20599a64e053ff5
itishprasad30 <itishprasad20@gmail.com>
2022-09-24 21:35:01 +0530

    Sorting by merger and quick sort

--- lectures/155-Programming-different/code.itish/src/DataStructures/DivideAndConqure/QuickSort.java
+++ lectures/155-Programming-different/code.itish/src/DataStructures/DivideAndConqure/QuickSort.java
@@ -1,4 +1,39 @@
 package DataStructures.DivideAndConqure;
 
+import java.util.Arrays;
+
 public class QuickSort {
+    public static void main(String[] args) {
+        int []arr = {6,3,8,9,7,5};
+        int n = arr.length;
+
+        quickSort(arr,0,n-1);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void quickSort(int []arr,int low,int high){
+        if(low< high) {
+            int pivotIdx = partition(arr, low, high);
+            quickSort(arr, low, pivotIdx - 1);
+            quickSort(arr, pivotIdx + 1, high);
+        }
+    }
+    static  int partition(int []arr,int low,int high){
+        int pivot = arr[high];
+        int i = low-1;
+
+        for (int j = low; j < high; j++) {
+            if(arr[j] < pivot){
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        return i;
+    }
 }
