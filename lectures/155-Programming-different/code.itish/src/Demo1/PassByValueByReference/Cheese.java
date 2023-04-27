aecf2981ca3402670d2642fbc8850cdbdcb4124e
itishprasad30 <itishprasad20@gmail.com>
2023-04-27 21:44:56 +0530

    Code added For Pass By Value and Pass By Reference

---
+++ lectures/155-Programming-different/code.itish/src/Demo1/PassByValueByReference/Cheese.java
@@ -0,0 +1,15 @@
+package Demo1.PassByValueByReference;
+
+import java.util.Objects;
+
+public class Cheese {
+    private  int cheese = 15;
+
+    public int getCheese() {
+        return cheese;
+    }
+
+    public void setCheese(int cheese) {
+        this.cheese = cheese;
+    }
+}
---
+++ lectures/155-Programming-different/code.itish/src/Demo1/PassByValueByReference/PassByValueAndRef.java
@@ -0,0 +1,18 @@
+package Demo1.PassByValueByReference;
+
+public class PassByValueAndRef {
+    // Java always 100% time pass by value
+    // but when a object that have value passes in a memory address
+    // so it can appear to be passing by reference
+
+    public static void main(String[] args) {
+        Cheese myCheese = new Cheese();
+        System.out.println(myCheese.getCheese());// 15
+        incrementCheese(myCheese);
+        System.out.println(myCheese.getCheese());// 20
+    }
+    public static void incrementCheese(Cheese cheese){
+        cheese = new Cheese();
+        cheese.setCheese(cheese.getCheese() + 5);
+    }
+}
